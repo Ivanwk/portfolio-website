d96648632021ded5d1cd2af736203ee8f4199f18
Ivan Wei Ket Yap <ivanweiket.yap@outlook.com>
2022-11-26 23:21:53 -0500

    Fixed nav bar on mobile

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,51 +11,53 @@ import { useState } from "react";
 const Nav = () => {
   const [activeNav, setActiveNav] = useState("#");
   return (
-    <nav>
-      <a
-        href="#home"
-        onClick={() => setActiveNav("#home")}
-        className={activeNav === "#home" ? "active" : ""}
-      >
-        <AiOutlineHome />
-      </a>
-      <a
-        href="#about"
-        onClick={() => setActiveNav("#about")}
-        className={activeNav === "#about" ? "active" : ""}
-      >
-        <AiOutlineUser />
-      </a>
-      <a
-        href="#experience"
-        onClick={() => setActiveNav("#experience")}
-        className={activeNav === "#experience" ? "active" : ""}
-      >
-        <BsBriefcase />
-      </a>
-      {/* <a
+    <div className="nav__container">
+      <nav className="nav_items">
+        <a
+          href="#home"
+          onClick={() => setActiveNav("#home")}
+          className={activeNav === "#home" ? "active" : ""}
+        >
+          <AiOutlineHome />
+        </a>
+        <a
+          href="#about"
+          onClick={() => setActiveNav("#about")}
+          className={activeNav === "#about" ? "active" : ""}
+        >
+          <AiOutlineUser />
+        </a>
+        <a
+          href="#experience"
+          onClick={() => setActiveNav("#experience")}
+          className={activeNav === "#experience" ? "active" : ""}
+        >
+          <BsBriefcase />
+        </a>
+        {/* <a
         href="#portfolio"
         onClick={() => setActiveNav("#portfolio")}
         className={activeNav === "#portfolio" ? "active" : ""}
       >
         <IoImageOutline />
       </a> */}
-      <a
-        href="#skills"
-        onClick={() => setActiveNav("#skills")}
-        className={activeNav === "#skills" ? "active" : ""}
-      >
-        <BsListCheck />
-      </a>
-      <a
-        href="#contact"
-        onClick={() => setActiveNav("#contact")}
-        className={activeNav === "#contact" ? "active" : ""}
-      >
-        <AiOutlineMessage />
-      </a>
-    </nav>
-  )
-}
+        <a
+          href="#skills"
+          onClick={() => setActiveNav("#skills")}
+          className={activeNav === "#skills" ? "active" : ""}
+        >
+          <BsListCheck />
+        </a>
+        <a
+          href="#contact"
+          onClick={() => setActiveNav("#contact")}
+          className={activeNav === "#contact" ? "active" : ""}
+        >
+          <AiOutlineMessage />
+        </a>
+      </nav>
+    </div>
+  );
+};
 
 export default Nav;
